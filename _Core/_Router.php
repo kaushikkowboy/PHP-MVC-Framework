6b85769f0ce3c6248e2c1d8a34a9f52b197b9a95
kaushikkowboy <kaushik.c555@gmail.com>
2017-05-21 05:44:43 -0700

    Adding regex in Front controller for url routing

--- _Core/_Router.php
+++ _Core/_Router.php
@@ -3,7 +3,16 @@ class _Router{
 protected $routes =[];
 
 //Add function will add routes in routing table
-public function add($route, $params){
+public function add($route, $params = []){
+	//Convert the route to a regular expression(escape forward slashes)
+	$route = preg_replace('/\//', '\\/', $route);
+	//convert variables {controller} {action}
+	$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+	//convert variables with custom regular expression {id:\d+}
+	$route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+	//Add start and end delimiters, and case censitive flag
+	$route = '/^'. $route . '$/i';
+
 	$this->routes[$route] =  $params;
 }
 
@@ -13,13 +22,20 @@ public function getRoutes(){
 }
 
 //Match the routes in routing table, if it's there then set $params
-public function match($url){
+public function match($url){ 
+	//$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
 	foreach ($this->routes as $route => $params) {
-		if($url == $route){
+		if(preg_match($route, $url, $matches)){
+			//$params = [];
+			foreach ($matches as $key => $match) {
+				if(is_string($key)){
+					$params[$key]= $match;
+				}
+			}
 			$this->params = $params;
 			return true;
 		}
-	}
+	}  
 	return false;
 }
 
--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
 
-require('../_Core/_Router.php');
+require('../_core/_Router.php');
 $router = new _Router;
 
 //Add some routes
 $router->add('',['controller' => 'Home', 'action' => 'index']); //Default home page
 $router->add('posts',['controller' => 'Posts', 'action' => 'index']); //Posts index fn
-$router->add('posts/new',['controller' => 'Posts', 'action' => 'new']); //Posts add fn
- 
+//$router->add('posts/new',['controller' => 'Posts', 'action' => 'new']); //Posts add fn
+$router->add('{controller}/{action}'); 
+$router->add('{controller}/{id:\d+}/{action}');
 $url = $_SERVER['QUERY_STRING'];
  
  //Checking The url and getting Routes from route table
